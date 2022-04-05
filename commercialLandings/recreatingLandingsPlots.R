ace34ee8adbb63fc13927a08d726963500e58a48
McMahon <Mike.McMahon@dfo-mpo.gc.ca>
2022-04-05 11:09:00 +0000

    adds flag to omit inverts

--- commercialLandings/recreatingLandingsPlots.R
+++ commercialLandings/recreatingLandingsPlots.R
@@ -1,9 +1,9 @@
-
+library(dplyr)
 setwd("C:/git/Maritimes/SOTO_Maritimes/")
 load("data/data_priv/SSdata/data/landings/landings.RData") #gets loaded as 'landings'
 source("C:/git/Maritimes/SOTO_Maritimes/commercialLandings/getCommercialSpp_SOTO.R")
 source("commercialLandings/getCommercialSpp_SOTO.R")
-
+omitBenthInverts <- T
 #limit to NAFOS we care about
 tomatch=c('4VN','4VS','4W','4X')
 landings = landings[ grep(paste(tomatch, collapse = '|'),landings$NAFO_UNIT),]
@@ -22,21 +22,25 @@ landings[landings$SPECIES %in% sppPelagic, "SPECIES_CATEGORY"] <- "Pelagic Fish"
 landings[landings$SPECIES %in% sppDemersal, "SPECIES_CATEGORY"] <- "Demersal Fish"
 landings[landings$SPECIES %in% sppBenthicInverts, "SPECIES_CATEGORY"] <- "Benthic Invertebrates"
 
+
 # # 4VN 1992 ~2000 demersal
 
 landings<-landings[,c("SPECIES_CATEGORY", "CATCH", "NAFO_GENERAL", "YEAR")]
 landings<-landings[!is.na(landings$SPECIES_CATEGORY),]
 landings<-landings[landings$YEAR >= 1970 & landings$YEAR < 2022, ]
 
+if (omitBenthInverts)landings<-landings[!landings$SPECIES_CATEGORY %in% "Benthic Invertebrates",]
 library(dplyr)
 all<- landings %>%
   group_by(YEAR, SPECIES_CATEGORY, NAFO_GENERAL) %>%
   summarise(TONNES = sum(CATCH), .groups = 'drop')
 rm(landings)
 
-
 all$SPECIES_CATEGORY <- factor(all$SPECIES_CATEGORY , levels=c("Benthic Invertebrates", "Pelagic Fish", "Demersal Fish") )
+if (omitBenthInverts)all$SPECIES_CATEGORY <- factor(all$SPECIES_CATEGORY , levels=c("Pelagic Fish", "Demersal Fish") )
 
+  
+  
 all_4VN <- all[all$NAFO_GENERAL == "4VN",]
 all_4VS <- all[all$NAFO_GENERAL == "4VS",]
 all_4W <- all[all$NAFO_GENERAL == "4W",]
@@ -55,10 +59,12 @@ makeThePlot <- function(data = NULL, title = NULL){
   maxD <- max(maxD1$annual) + 0.05*max(maxD1$annual)
   yBreaks<- pretty(x = c(0,maxD1$annual), min.n = 4, n=8)
 
+  colors <- c("#7E62A1","#9BBB59","#C0514E")
+  if (omitBenthInverts)colors <- c("#9BBB59","#C0514E")
   p <- ggplot(data = data, aes(fill=SPECIES_CATEGORY, y=TONNES, x=YEAR)) +
     ggtitle(title) +
     geom_bar(position='stack', stat='identity')+ylab("Landings (tonnes)") + xlab("") +
-    scale_fill_manual(values = c("#7E62A1","#9BBB59","#C0514E")) +
+    scale_fill_manual(values = colors) +
     scale_y_continuous(breaks=yBreaks,labels = function(x) format(x, scientific = FALSE), expand = c(0, 0),limits=c(0,maxD))+ 
     scale_x_continuous(breaks=xMin, labels = xlabels, expand=c(0,0)) +
     theme(legend.position = c(0.82,0.85), legend.title=element_blank(), plot.title = element_text(vjust = - 8, hjust = 0.5),  
